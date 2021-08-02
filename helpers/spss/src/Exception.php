4e93c00da6da1a3f399827cd5d3b72ee33f9fd74
Adam Zammit <adam.zammit@acspri.org.au>
2021-08-02 11:08:39 +1000

    Remove checked out code

--- helpers/spss/src/Exception.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace SPSS;
-
-class Exception extends \RuntimeException
-{
-}
--- helpers/spss/src/Sav/Record/Info/DataFileAttributes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SPSS\Sav\Record\Info;
-
-class DataFileAttributes extends Unknown
-{
-    const SUBTYPE = 17;
-}
